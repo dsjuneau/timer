42d7001a5277b77bfda3eac2ee123893265422d0
Scott Juneau <dsjuneau@gmail.com>
2021-01-21 07:49:34 -0600

    bars are working

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ let hour = 8;
 let minute = 30;
 let second = 0;
 let current = 0;
+let interval = 0;
+let inInterval = 0;
 setInterval(() => {
   //replace this section with a timer once everything is working
 
@@ -33,4 +35,71 @@ setInterval(() => {
   // end of this section to replace with timer once everything is working
 
   current = (hour - 8) * 3600 + (minute - 30) * 60 + second - 0;
+  interval = Math.floor(current / 1800);
+  inInterval = current - interval * 1800;
+
+  if (current > 1000) {
+    timer.style.borderLeft = "none";
+    timer.style.borderRight = "1px solid white";
+  } else {
+    timer.style.marginLeft = (current * 100) / (13 * 1800) + "%";
+  }
+
+  switch (interval) {
+    case 0:
+      t1.style.visibility = "visible";
+      t1.style.width = inInterval / 18 + "%";
+      break;
+    case 1:
+      t2.style.visibility = "visible";
+      t2.style.width = inInterval / 18 + "%";
+      break;
+    case 2:
+      t3.style.visibility = "visible";
+      t3.style.width = inInterval / 18 + "%";
+      break;
+    case 3:
+      t4.style.visibility = "visible";
+      t4.style.width = inInterval / 18 + "%";
+      break;
+    case 4:
+      t5.style.visibility = "visible";
+      t5.style.width = inInterval / 18 + "%";
+      break;
+    case 5:
+      t6.style.visibility = "visible";
+      t6.style.width = inInterval / 18 + "%";
+      break;
+    case 6:
+      t7.style.visibility = "visible";
+      t7.style.width = inInterval / 18 + "%";
+      break;
+    case 7:
+      t8.style.visibility = "visible";
+      t8.style.width = inInterval / 18 + "%";
+      break;
+    case 8:
+      t9.style.visibility = "visible";
+      t9.style.width = inInterval / 18 + "%";
+      break;
+    case 9:
+      t10.style.visibility = "visible";
+      t10.style.width = inInterval / 18 + "%";
+      break;
+    case 10:
+      t11.style.visibility = "visible";
+      t11.style.width = inInterval / 18 + "%";
+      break;
+    case 11:
+      t12.style.visibility = "visible";
+      t12.style.width = inInterval / 18 + "%";
+      break;
+    case 12:
+      t13.style.visibility = "visible";
+      t13.style.width = inInterval / 18 + "%";
+      break;
+
+    default:
+      break;
+  }
 }, 10);
